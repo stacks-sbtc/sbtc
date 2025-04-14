2d1e47b5bbede14f47410ac5e236b8605e70c7fb
Viktar Makouski <viktarmakouski@yandex.ru>
2025-04-14 15:08:30 +0300

    add new condition to should_coordinate_dkg

--- signer/src/transaction_coordinator.rs
+++ signer/src/transaction_coordinator.rs
@@ -2540,6 +2540,21 @@ pub async fn should_coordinate_dkg(
     let storage = context.get_storage();
     let config = context.config();
 
+    // Trigger dkg if signer set changes
+    let last_dkg_signer_set: BTreeSet<_> = context
+        .state()
+        .current_signer_set()
+        .get_signers()
+        .iter()
+        .map(|signer| signer.public_key().clone())
+        .collect();
+    let config_signer_set = config.signer.bootstrap_signing_set();
+
+    if last_dkg_signer_set != config_signer_set {
+        tracing::info!("signer set has changed, proceeding with DKG");
+        return Ok(true);
+    }
+
     // Get the number of DKG shares that have been stored
     let dkg_shares_entry_count = storage.get_encrypted_dkg_shares_count().await?;
 
