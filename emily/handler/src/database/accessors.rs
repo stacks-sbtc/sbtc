513a61a50054812dfac3501c5144e26cef3262e0
Viktar Makouski <viktarmakouski@yandex.ru>
2025-04-24 12:26:29 +0300

    fmt

--- emily/handler/src/database/accessors.rs
+++ emily/handler/src/database/accessors.rs
@@ -844,7 +844,9 @@ pub async fn get_limits(context: &EmilyContext) -> Result<Limits, Error> {
         global_cap.rolling_withdrawal_blocks,
         global_cap.rolling_withdrawal_cap,
     )
-    .await.ok().flatten();
+    .await
+    .ok()
+    .flatten();
 
     // Get the global limit for the whole thing.
     Ok(Limits {
