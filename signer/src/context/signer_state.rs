34b3515a71669bb986a5ef9c3996bbc57e037744
djordon <dan.jordon@gmail.com>
2025-06-05 16:41:44 -0400

    WIP update signer state object to take a signer set info object

--- signer/src/context/signer_state.rs
+++ signer/src/context/signer_state.rs
@@ -3,13 +3,14 @@
 use std::collections::BTreeSet;
 use std::sync::{
     RwLock,
-    atomic::{AtomicBool, AtomicU16, AtomicU64, Ordering},
+    atomic::{AtomicBool, AtomicU64, Ordering},
 };
 
 use bitcoin::Amount;
 use hashbrown::HashSet;
 use libp2p::PeerId;
 
+use crate::block_observer::SignerSetInfo;
 use crate::keys::PublicKey;
 use crate::storage::model::BitcoinBlockHash;
 use crate::storage::model::BitcoinBlockHeight;
@@ -22,8 +23,7 @@ use crate::storage::model::BitcoinBlockRef;
 pub struct SignerState {
     current_signer_set: SignerSet,
     current_limits: RwLock<SbtcLimits>,
-    current_aggregate_key: RwLock<Option<PublicKey>>,
-    current_signatures_required: AtomicU16,
+    registry_signing_set_info: RwLock<Option<SignerSetInfo>>,
     sbtc_contracts_deployed: AtomicBool,
     sbtc_bitcoin_start_height: AtomicU64,
     is_sbtc_bitcoin_start_height_set: AtomicBool,
@@ -39,46 +39,52 @@ impl SignerState {
     }
 
     /// Get the current number of signatures required.
-    pub fn current_signatures_required(&self) -> u16 {
-        self.current_signatures_required.load(Ordering::SeqCst)
-    }
-
-    /// Set the current number of signatures required.
-    pub fn set_current_signatures_required(&self, signatures_required: u16) {
-        self.current_signatures_required
-            .store(signatures_required, Ordering::SeqCst);
+    pub fn registry_signatures_required(&self) -> Option<u16> {
+        self.registry_signing_set_info
+            .read()
+            .expect("")
+            .as_ref()
+            .map(|info| info.signatures_required)
     }
 
     /// Return the public keys of the current signer set.
-    pub fn current_signer_public_keys(&self) -> BTreeSet<PublicKey> {
-        self.current_signer_set
-            .get_signers()
-            .into_iter()
-            .map(|signer| signer.public_key)
-            .collect()
+    pub fn registry_current_signer_set(&self) -> Option<BTreeSet<PublicKey>> {
+        self.registry_signing_set_info
+            .read()
+            .expect("")
+            .as_ref()
+            .map(|signer| signer.signer_set.clone())
+    }
+
+    /// Replace the current signer set with the given set of public keys.
+    pub fn update_registry_signer_set_info(&self, info: SignerSetInfo) {
+        self.registry_signing_set_info
+            .write()
+            .expect("BUG: Failed to acquire read lock")
+            .replace(info);
+    }
+
+    /// Return the aggregate key currently stored in the smart contract.
+    pub fn registry_current_aggregate_key(&self) -> Option<PublicKey> {
+        self.registry_signing_set_info
+            .read()
+            .expect("")
+            .as_ref()
+            .map(|signer| signer.aggregate_key)
+    }
+
+    /// Return the aggregate key currently stored in the smart contract.
+    pub fn registry_signer_set_info(&self) -> Option<SignerSetInfo> {
+        self.registry_signing_set_info
+            .read()
+            .expect("")
+            .as_ref()
+            .cloned()
     }
 
     /// Replace the current signer set with the given set of public keys.
     pub fn update_current_signer_set(&self, public_keys: BTreeSet<PublicKey>) {
         self.current_signer_set.replace_signers(public_keys);
-    }
-
-    /// Return the current aggregate key from the cache.
-    #[allow(clippy::unwrap_in_result)]
-    pub fn current_aggregate_key(&self) -> Option<PublicKey> {
-        self.current_aggregate_key
-            .read()
-            .expect("BUG: Failed to acquire read lock")
-            .as_ref()
-            .copied()
-    }
-
-    /// Set the current aggregate key to the given public key.
-    pub fn set_current_aggregate_key(&self, aggregate_key: PublicKey) {
-        self.current_aggregate_key
-            .write()
-            .expect("BUG: Failed to acquire write lock")
-            .replace(aggregate_key);
     }
 
     /// Get the current bitcoin chain tip.
@@ -151,9 +157,8 @@ impl Default for SignerState {
     fn default() -> Self {
         Self {
             current_signer_set: Default::default(),
-            current_signatures_required: AtomicU16::new(0),
             current_limits: RwLock::new(SbtcLimits::zero()),
-            current_aggregate_key: RwLock::new(None),
+            registry_signing_set_info: RwLock::new(None),
             sbtc_contracts_deployed: Default::default(),
             sbtc_bitcoin_start_height: Default::default(),
             is_sbtc_bitcoin_start_height_set: Default::default(),
