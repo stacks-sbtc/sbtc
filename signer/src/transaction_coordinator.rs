b41e84eba0fb2cb0e4cba310c2d799c0f164e703
jesus <jnajera1917@gmail.com>
2025-07-21 08:37:58 -0400

    borrow fix

--- signer/src/transaction_coordinator.rs
+++ signer/src/transaction_coordinator.rs
@@ -2547,7 +2547,9 @@ pub async fn should_coordinate_dkg(
     // instead of doing new DKG rounds. If we fail to do so they will eventually
     // be marked as failed, and we will resume DKG-ing.
     let latest_dkg_shares = storage.get_latest_encrypted_dkg_shares().await?;
-    if latest_dkg_shares.map(|s| s.dkg_shares_status) == Some(model::DkgSharesStatus::Unverified) {
+    if latest_dkg_shares.as_ref().map(|s| s.dkg_shares_status)
+        == Some(model::DkgSharesStatus::Unverified)
+    {
         tracing::debug!("latest shares are unverified; skipping DKG");
         return Ok(false);
     }
