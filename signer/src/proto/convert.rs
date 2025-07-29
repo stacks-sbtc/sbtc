8ff8523acd32b353307876d4b996a3b303e1a6f1
Joey Yandle <xoloki@gmail.com>
2025-07-29 09:49:47 -0400

    use explicit point at infinity for missing kex_public_key and add comment

--- signer/src/proto/convert.rs
+++ signer/src/proto/convert.rs
@@ -1603,7 +1603,8 @@ impl TryFrom<proto::SignerDkgPublicShares> for DkgPublicShares {
     fn try_from(value: proto::SignerDkgPublicShares) -> Result<Self, Self::Error> {
         let kex_public_key = match value.kex_public_key {
             Some(key) => Point::try_from(key)?,
-            None => Point::new(),
+            // use point at infinity since this value will fail deserialization
+            None => Point::identity(),
         };
         Ok(DkgPublicShares {
             kex_public_key,
