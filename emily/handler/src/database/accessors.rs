1aefc6d45af8335d23deb06c87da3c59a470e75b
Viktar Makouski <mcviktor@tut.by>
2025-11-06 16:57:40 +0300

    rm typo from log
    Co-authored-by: Matteo Almanza <matteojug@gmail.com>

--- emily/handler/src/database/accessors.rs
+++ emily/handler/src/database/accessors.rs
@@ -349,7 +349,7 @@ pub async fn get_withdrawal_entry(
             }
 
             if in_canonical_chain.len() == 1 {
-                // SAFETY: The collection is guaranteed to have one item per the length check immediately above.`
+                // SAFETY: The collection is guaranteed to have one item per the length check immediately above.
                 return Ok(in_canonical_chain[0].clone());
             }
             if in_canonical_chain.is_empty() {
