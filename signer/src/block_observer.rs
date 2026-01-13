0c61eb945055d24dcf82221c73bc626ce9963f0f
djordon <dan.jordon@gmail.com>
2026-01-13 22:41:17 +0000

    Okay, this can cause problems if some test fails

--- signer/src/block_observer.rs
+++ signer/src/block_observer.rs
@@ -369,8 +369,6 @@ impl<C: Context, B> BlockObserver<C, B> {
         let block_headers = self.next_headers_to_process(block_hash).await?;
 
         let chain_tip = block_headers.last().cloned();
-        // This should be the header for the above block hash.
-        debug_assert_eq!(chain_tip.as_ref().map(|h| h.hash), Some(block_hash));
 
         for block_header in block_headers {
             self.process_bitcoin_block(block_header).await?;
