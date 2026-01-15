affe2219a5a3d6f9a2b281082a25151c91cda4a6
Viktar Makouski <viktarmakouski@yandex.ru>
2026-01-15 18:38:53 +0300

    last? expectation change

--- emily/handler/tests/integration/withdrawal.rs
+++ emily/handler/tests/integration/withdrawal.rs
@@ -1472,7 +1472,7 @@ async fn expected_fulfillment_info_stored_correctly() {
         .unwrap()
         .unwrap();
 
-    assert_eq!(7, received_expected_height);
+    assert_eq!(8, received_expected_height);
     assert_eq!(expected_expected_txid, received_expected_txid);
 
     clean_test_setup(tables).await;
