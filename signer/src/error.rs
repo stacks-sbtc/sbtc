d2948a635f288296f6e7a74a00f7d4ba69a6e93c
Viktar Makouski <viktarmakouski@yandex.ru>
2026-02-02 13:05:07 +0300

    rm 404 related error checks

--- signer/src/error.rs
+++ signer/src/error.rs
@@ -810,35 +810,4 @@ impl Error {
     pub fn wsts_coordinator(err: wsts::state_machine::coordinator::Error) -> Self {
         Error::WstsCoordinator(Box::new(err))
     }
-
-    /// Checks if given error represents a 404 response from Stacks node
-    /// (directly or via FallbackClient).
-    /// In case of fallback client, this function treats an error as 404 only
-    /// if all Stacks nodes in fallback client returned 404.
-    pub fn is_stacks_node_response_404(&self) -> bool {
-        is_stacks_node_response_fallback_client_error_404(self)
-            || is_stacks_node_response_simple_client_error_404(self)
-    }
-}
-
-/// Checks if given error is a 404 error response from Stacks node
-/// with fallback client (all nodes returned 404)
-fn is_stacks_node_response_fallback_client_error_404(error: &Error) -> bool {
-    if let Error::FallbackClient(FallbackClientError::AllClientsFailed(inners)) = error {
-        return inners
-            .iter()
-            .all(is_stacks_node_response_simple_client_error_404);
-    }
-    false
-}
-
-/// Checks if given error is a 404 error response from Stacks node
-fn is_stacks_node_response_simple_client_error_404(error: &Error) -> bool {
-    if let Error::StacksNodeResponse(resp) = error
-        && let Some(status) = resp.status()
-        && status == reqwest::StatusCode::NOT_FOUND
-    {
-        return true;
-    }
-    false
 }
