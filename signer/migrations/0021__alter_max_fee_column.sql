b20211676d2ae6e88a5c8a110fa0c0fcadeb279b
djordon <dan.jordon@gmail.com>
2026-02-13 01:36:17 +0000

    dont use int8send, use a more standard migration query

--- signer/migrations/0021__alter_max_fee_column.sql
+++ signer/migrations/0021__alter_max_fee_column.sql
@@ -1,11 +1,12 @@
--- Add new bytea column for max_fee on deposit_requests, backfill from BIGINT, then replace column.
+-- Add new bytea column for max_fee on deposit_requests, backfill from
+-- BIGINT, then replace the existing max_fee column with the new version.
 
 ALTER TABLE sbtc_signer.deposit_requests
     ADD COLUMN max_fee_bytes BYTEA;
 
--- Convert existing max_fee (BIGINT) to 8-byte big-endian bytea via int8send.
+-- Convert existing max_fee (BIGINT) to an 8-byte big-endian bytea.
 UPDATE sbtc_signer.deposit_requests
-SET max_fee_bytes = int8send(max_fee);
+SET max_fee_bytes = decode(lpad(to_hex(max_fee), 16, '0'), 'hex');
 
 ALTER TABLE sbtc_signer.deposit_requests
     ALTER COLUMN max_fee_bytes SET NOT NULL;
