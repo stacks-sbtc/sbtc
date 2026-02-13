8b6b60a8990b78f034cbc098415386a3dab737a7
djordon <dan.jordon@gmail.com>
2026-02-13 00:33:13 +0000

    store the max fee as bytes

---
+++ signer/migrations/0021__alter_max_fee_column.sql
@@ -0,0 +1,13 @@
+-- Add new bytea column for max_fee on deposit_requests, backfill from BIGINT, then replace column.
+
+ALTER TABLE sbtc_signer.deposit_requests
+    ADD COLUMN max_fee_bytes BYTEA;
+
+-- Convert existing max_fee (BIGINT) to 8-byte big-endian bytea via int8send.
+UPDATE sbtc_signer.deposit_requests
+SET max_fee_bytes = int8send(max_fee);
+
+ALTER TABLE sbtc_signer.deposit_requests
+    DROP COLUMN max_fee,
+    RENAME COLUMN max_fee_bytes TO max_fee,
+    ALTER COLUMN max_fee SET NOT NULL;
