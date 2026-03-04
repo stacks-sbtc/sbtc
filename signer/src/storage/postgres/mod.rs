9198b2efbf5ea72224713cd413334c1ec071b4b4
Daniel Jordon <djordon@users.noreply.github.com>
2026-03-04 14:20:24 -0500

    fix: handle any max fee that can fit in a `u64` (#1927)
    ## Description
    
    Closes: https://github.com/stacks-sbtc/sbtc/issues/1925
    
    I prefer these changes over the ones in
    https://github.com/stacks-sbtc/sbtc/pull/1926.
    
    ## Changes
    
    * Have Emily only return deposit requests where the max-fee was less
    than `i64::MAX`. This is here until the next release where we have
    shipped the signer's changes.
    * Update the signer to be able to process any max-fee by changing the
    column that stores the max-fee.
    
    ## Testing Information
    
    ## Checklist
    
    - [x] I have performed a self-review of my code

--- signer/src/storage/postgres/mod.rs
+++ signer/src/storage/postgres/mod.rs
@@ -8,5 +8,5 @@ pub use store::PgStore;
 pub use store::PgTransaction;
 
 /// All migration scripts from the `signer/migrations` directory.
-static PGSQL_MIGRATIONS: include_dir::Dir =
+pub static PGSQL_MIGRATIONS: include_dir::Dir =
     include_dir::include_dir!("$CARGO_MANIFEST_DIR/migrations");
