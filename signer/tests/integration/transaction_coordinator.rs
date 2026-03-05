ab80135d317999a51edec3559233e1bc1c674fc5
djordon <dan.jordon@gmail.com>
2026-03-05 07:03:17 +0000

    this sleep may matter?

--- signer/tests/integration/transaction_coordinator.rs
+++ signer/tests/integration/transaction_coordinator.rs
@@ -300,7 +300,7 @@ async fn wait_for_tenure_completed<S>(
 
     // It's not entirely clear why this sleep is helpful, but it appears to
     // be necessary in CI.
-    Sleep::for_secs(1).await;
+    Sleep::for_secs(2).await;
 }
 
 fn mock_deploy_all_contracts() -> Box<dyn FnOnce(&mut MockStacksInteract)> {
