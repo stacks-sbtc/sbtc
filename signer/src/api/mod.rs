e9ff126cd83f80e1ccd23d6100f5f47be35aa168
Matteo Almanza <matteo.almanza@algorand.com>
2026-03-20 18:57:40 +0100

    feat: add withdrawal e2e test

--- signer/src/api/mod.rs
+++ signer/src/api/mod.rs
@@ -2,7 +2,7 @@
 //!
 
 mod info;
-mod new_block;
+pub mod new_block;
 mod router;
 mod status;
 
@@ -18,4 +18,4 @@ pub struct ApiState<C> {
 }
 
 /// The name of the sbtc registry smart contract.
-const SBTC_REGISTRY_CONTRACT_NAME: &str = "sbtc-registry";
+pub const SBTC_REGISTRY_CONTRACT_NAME: &str = "sbtc-registry";
