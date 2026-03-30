19e74dfbddfa3e10f183e2c03290aa8faffef57b
djordon <dan.jordon@gmail.com>
2026-03-30 14:37:58 +0000

    just use warp::http::StatusCode like before

--- emily/handler/src/api/handlers/deposit.rs
+++ emily/handler/src/api/handlers/deposit.rs
@@ -712,17 +712,11 @@ fn validate_reclaim_pubkeys(reclaim_pubkeys: &str) -> Result<Vec<[u8; 32]>, Erro
         .map(|s| {
             hex::decode(s)
                 .map_err(|_| {
-                    Error::HttpRequest(
-                        reqwest::StatusCode::BAD_REQUEST,
-                        "invalid pubkey".to_string(),
-                    )
+                    Error::HttpRequest(StatusCode::BAD_REQUEST, "invalid pubkey".to_string())
                 })
                 .and_then(|bytes| {
                     bytes.try_into().map_err(|_| {
-                        Error::HttpRequest(
-                            reqwest::StatusCode::BAD_REQUEST,
-                            "invalid pubkey".to_string(),
-                        )
+                        Error::HttpRequest(StatusCode::BAD_REQUEST, "invalid pubkey".to_string())
                     })
                 })
         })
