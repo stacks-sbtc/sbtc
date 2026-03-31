dc2d445488d92675544a928a04b022a7b2a1dd4b
Matteo Almanza <matteo.almanza@algorand.com>
2026-03-31 03:19:14 +0200

    feat: add tests for fee retry logic

--- signer/src/bitcoin/utxo.rs
+++ signer/src/bitcoin/utxo.rs
@@ -72,14 +72,14 @@ const DEFAULT_INCREMENTAL_RELAY_FEE_RATE: f64 =
 /// of the signers' input UTXO and a UTXO for a deposit request. The output
 /// is the signers' new UTXO. The deposit request is such that the sweep
 /// transaction has the largest size of solo deposit sweep transactions.
-const SOLO_DEPOSIT_TX_VSIZE: f64 = 249.0;
+pub const SOLO_DEPOSIT_TX_VSIZE: f64 = 249.0;
 
 /// This constant represents the virtual size (in vBytes) of a BTC
 /// transaction servicing only one withdrawal request, except the
 /// withdrawal output is not in the transaction. This way the sweep
 /// transaction's OP_RETURN output is the right size, and we can handle the
 /// variability of output sizes.
-const BASE_WITHDRAWAL_TX_VSIZE: f64 = MAX_BASE_TX_VSIZE as f64;
+pub const BASE_WITHDRAWAL_TX_VSIZE: f64 = MAX_BASE_TX_VSIZE as f64;
 
 /// This constant represents the maximum virtual size (in vBytes) of a BTC
 /// transaction excluding withdrawals outputs and deposit inputs.
