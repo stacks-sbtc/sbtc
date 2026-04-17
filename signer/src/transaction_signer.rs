6d264752681ff4c99d7f5baa7668a4b07a9c6558
Matteo Almanza <matteo@stackslabs.com>
2026-04-17 18:13:11 +0200

    feat: add reclaim to demo-cli (#2011)
    ## Description
    
    Closes: #?
    
    ## Changes
    
    Add reclaim command to demo-cli (used to test spox).
    
    Also some random fixes:
     - Fix `make devenev-sbtc-*` commands
     - Fix `demo-cli` to use the non-follower bitcoin node
     - Small nit on a log
    
    ## Testing Information
    
    Reclaim can be tested with:
    ```bash
    # After first DKG
    cargo run -p signer --bin demo-cli deposit --amount 3000000 --lock-time 1
    cargo run -p signer --bin demo-cli reclaim --txid <tx from above>
    ```
    
    ## Checklist
    
    - [ ] I have performed a self-review of my code
    - [ ] My changes generate no new warnings
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged and published in downstream
    modules

--- signer/src/transaction_signer.rs
+++ signer/src/transaction_signer.rs
@@ -208,14 +208,14 @@ impl std::fmt::Display for StacksSignRequestId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             StacksSignRequestId::CompleteDeposit(outpoint) => {
-                write!(f, "CompleteDeposit({outpoint}")
+                write!(f, "CompleteDeposit({outpoint})")
             }
             StacksSignRequestId::CompleteWithdrawal(request_id) => {
-                write!(f, "CompleteWithdrawal({request_id}")
+                write!(f, "CompleteWithdrawal({request_id})")
             }
-            StacksSignRequestId::RotateKeys(public_key) => write!(f, "RotateKeys({public_key}"),
+            StacksSignRequestId::RotateKeys(public_key) => write!(f, "RotateKeys({public_key})"),
             StacksSignRequestId::SmartContract(smart_contract) => {
-                write!(f, "SmartContract({smart_contract}")
+                write!(f, "SmartContract({smart_contract})")
             }
         }
     }
