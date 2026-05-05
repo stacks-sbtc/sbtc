65876d22e1740c2ec3cd735e700338ce3b2d8784
Matteo Almanza <matteo@stackslabs.com>
2026-05-05 11:34:18 +0200

    Revert "chore: update openapi generator (#1986)"
    This reverts commit 9489b856584b8af3dcf0f22b692d18822dc6ad1d.

--- signer/src/error.rs
+++ signer/src/error.rs
@@ -710,7 +710,7 @@ pub enum Error {
 
     /// Could not create reqwest client
     #[error("we received an error when creating the Emily's reqwest client: {0}")]
-    EmilyReqwestClientCreation(#[source] reqwest::Error),
+    EmilyReqwestClientCreation(#[source] reqwest_012::Error),
 
     /// This happens during the validation of a stacks transaction when the
     /// current signer is not a member of the signer set indicated by the
